cf9b89d66543ffaf4d6fd2e557dd32a1b8bb4c8d
Rodrigo Sumioshi <shodirodrigo@gmail.com>
2024-05-20 00:22:22 -0300

    removendo de submodulo

---
+++ frontend/src/main.js
@@ -0,0 +1,31 @@
+document.getElementById('loginForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    try {
+        const response = await fetch('http://127.0.0.1:3002/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ nome: email, senha: password })
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data.message);
+            // Redirecionar para a página principal ou fazer outra ação após o login bem-sucedido
+            redirectToIndex();
+        } else {
+            console.log("Login falhou!");
+        }
+    } catch (error) {
+        console.error('Erro ao fazer login:', error);
+    }
+});
+
+function redirectToIndex() {
+    window.location.href = "index.html";
+}
